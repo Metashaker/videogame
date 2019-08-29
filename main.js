b2d863b9752af1592608fb3f5536132ca33aca84
Emiliano <emilianopb92@gmail.com>
2019-08-29 14:47:41 -0500

    Starts to work

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //VARIABLEs DEFINITION
 
 //0.- Animation and  canvas
-let targetsArr = []
+let targets = []
+let score = 0
 let frames = 0
 let interval
 const canvas = document.querySelector('canvas')
@@ -36,6 +37,12 @@ class Player1 {
  moveUp() {
   this.y -= 10
 }
+moveLeft() {
+  this.x -= 10
+}
+moveRight() {
+  this.x += 10
+}
   top() {
     return this.y
   }
@@ -55,6 +62,7 @@ class Player1 {
       this.y < target.y + target.height &&
       this.y + this.height > target.y
     )
+
   }
 }
 //1.- Objects
@@ -66,10 +74,21 @@ class Target {
     this.width = 20
     this.img = new Image()
     this.img.src = img
-
   }
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
+  top() {
+    return this.y
+  }
+  bottom() {
+    return this.y + this.height
+  }
+  left() {
+    return this.x
+  }
+  right() {
+    return this.x + this.width
   }
   //moveTarget() {
   //  x = Math.round(Math.random() * canvas.width - targetLength);
@@ -77,31 +96,8 @@ class Target {
   //}
   }
 
-const generateTarget = () => {
-  target = new Array()
-  target[0] = 'assets/pill.png'
-  target[1] = 'assets/sick-taco_burned.png'
-  target[2] = 'assets/pill.png'
 
-  let x = canvas.width
-  let y = canvas.height
-
-  if (frames % 200  === 0 ) {
-    let randX = Math.floor((Math.random() * x ) + 5)
-    let randY = Math.floor((Math.random() * y ) + 5)
-    let randTarget = Math.floor(Math.random() * 2 )
-    target.push(new Target(randTarget, randX, randY))
-  }
-
-}
-
-let drawTarget = () => {
-  targetsArr.forEach(target => {
-    target.draw()
-  })
-}
 //Funcion que genere imagen aleatotria, y la posicione en x y y aleatoriamente
-const pill = new Target ('assets/pill.png', 200, 500)
 const player1 = new Player1('assets/ness-run-right.png',10,100)
 
 //EVENT LISTENERS
@@ -116,6 +112,12 @@ document.onkeydown = e => {
       case 40: 
           player1.moveDown()
           break;
+      case 37:
+        player1.moveLeft()
+        break;
+      case 39:
+        player1.moveRight()
+        break;
     }
   }
 
@@ -123,7 +125,7 @@ document.onkeydown = e => {
 //1.- Event 1 changes background, draws screen and Ness, and gives Ness functionality
 startButton.addEventListener('click', () => {
   //Change background and appear canvas
-  gameArea.style.backgroundImage = 'url(assets/taco_truck_illustration-01_2x.jpg)'
+  canvas.style.backgroundImage = 'url(assets/taco_truck_illustration-01_2x.jpg)'
   canvas.style.display = 'block'
   //Disappear start screen elements
   startButton.style.display = 'none'
@@ -132,13 +134,15 @@ startButton.addEventListener('click', () => {
   document.querySelector('.background > img').style.display = 'none'
   //Make Ness appear over Ironhack adress and draw map
   player1.draw()
+  gameLoop()
+
 })
 
 
 //ACTIVATION AND HELPER FUNCTIONS
 
 const startGame = () => {
-  interval = setInterval(gameLoop, 30)
+  interval = setInterval(gameLoop, 20)
  }
  const stopGame = () => {
    clearInterval(interval)
@@ -148,6 +152,45 @@ const startGame = () => {
  const clearCanvas = () => {
    ctx.clearRect(0,0, canvas.width, canvas.height)
  }
+ const generateTarget = () => {
+  const yolo = ['assets/pill.png',
+  'assets/sick-taco_burned.png',
+  'assets/pill.png']
+
+  let x = canvas.width
+  let y = canvas.height
+
+  if (frames % 3000  === 0 ) {
+    let randX = Math.floor((Math.random() * x ) + 5)
+    let randY = Math.floor((Math.random() * y ) + 5)
+    let randTarget = Math.floor(Math.random() * 2 )
+    targets.push(new Target(yolo[randTarget], randX, randY))
+  }
+}
+
+const drawTarget = () => {
+  if(targets.length !== 0){targets.forEach(element => {
+    element.draw()
+  })}
+}
+const getTarget = () => {
+  targets.forEach(( element => {
+    if (player1.isTouching(element)) {
+      targets.slice(element, 1)
+      score += 1
+      console.log('hiiiii')
+      targets.slice(element, 1)
+    }
+  }))
+}
+function drawScore() {
+
+  ctx.fillStyle = 'black'
+  ctx.font = '20px Courier'
+  ctx.fillText(`Score: ${score}`, 10, 10)
+  }
+
+
 
 function gameLoop() {
       frames += 1
@@ -156,10 +199,12 @@ function gameLoop() {
       player1.draw()
       generateTarget()
       drawTarget()
+      getTarget()
+      drawScore()
 
     //drawObstacles()
     //updateObstacles()
     }
 
-gameLoop()
+
 
