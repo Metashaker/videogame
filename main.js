551bc00d67c145082272db3e5f8cd35a0a7edfbb
Emiliano <emilianopb92@gmail.com>
2019-08-29 11:39:59 -0500

    Not working on Chrome

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-//VARIABLE DEFINITION
+//VARIABLEs DEFINITION
 
 //0.- Animation and  canvas
-let levels
+let targetsArr = []
 let frames = 0
 let interval
 const canvas = document.querySelector('canvas')
@@ -11,8 +11,7 @@ const startButton = document.querySelector('.start-button')
 
 
 //2.-Screens
-const gameIntro = document.querySelector('.game-intro')
-const map = document.querySelector('.map')
+const gameArea = document.querySelector('.background')
 
 
 
@@ -20,31 +19,22 @@ const map = document.querySelector('.map')
 
 //0.- Characters
 class Player1 {
-  constructor(x, y) {
+  constructor(img, x, y) {
     this.x = x
     this.y = y
     this.width = 40
     this.height = 40
     this.img = new Image()
-    this.img.src =
-      './assets/ness-run-right.png'
+    this.img.src = img
   }
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
   }
-  mapMovementDown () {
-    this.x -= 2.5
-    this.y +=8
-  }
-  mapMovementUp () {
-    this.x+=2.5
-    this.y -=8
-  } 
- moveRight() {
-   this.x +=10
+ moveDown() {
+   this.y +=10
  }
- moveLeft() {
-  this.x -= 10
+ moveUp() {
+  this.y -= 10
 }
   top() {
     return this.y
@@ -58,66 +48,97 @@ class Player1 {
   right() {
     return this.x + this.width
   }
-  moveRight(){
-    this.x += 20
-  }
-  moveLeft(){
-    this.x -= 20
-  }
-  isTouching(obstacle) {
+  isTouching(target) {
     return (
-      this.x < obstacle.x + obstacle.width &&
-      this.x + this.width > obstacle.x &&
-      this.y < obstacle.y + obstacle.height &&
-      this.y + this.height > obstacle.y
+      this.x < target.x + target.width &&
+      this.x + this.width > target.x &&
+      this.y < target.y + target.height &&
+      this.y + this.height > target.y
     )
   }
 }
+//1.- Objects
+class Target {
+  constructor (img, x, y) {
+    this.x = x
+    this.y = y
+    this.height = 20
+    this.width = 20
+    this.img = new Image()
+    this.img.src = img
 
-
-const player1 = new Player1(116,60)
-//EVENT LISTENERS
-document.onkeydown = e => {
-  if (document.querySelector('.screen2').style.display === 'block') {
-    switch(e.keyCode){
-      case 37:
-          player1.mapMovementUp()
-          break;
-      case 39: 
-          player1.mapMovementDown()
-          break;
-    }
-  } else
-  switch(e.keyCode){
-    case 37:
-        player1.moveLeft()
-        break;
-    case 39: 
-        player1.moveRight()
-        break;
   }
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
+  //moveTarget() {
+  //  x = Math.round(Math.random() * canvas.width - targetLength);
+  //  y = Math.round(Math.random() * canvas.height - targetLength)
+  //}
+  }
+
+const generateTarget = () => {
+  target = new Array()
+  target[0] = 'assets/pill.png'
+  target[1] = 'assets/sick-taco_burned.png'
+  target[2] = 'assets/pill.png'
+
+  let x = canvas.width
+  let y = canvas.height
+
+  if (frames % 200  === 0 ) {
+    let randX = Math.floor((Math.random() * x ) + 5)
+    let randY = Math.floor((Math.random() * y ) + 5)
+    let randTarget = Math.floor(Math.random() * 2 )
+    target.push(new Target(randTarget, randX, randY))
+  }
+
 }
 
-//1.- Event 1 changes background, draws map and Ness, and gives Ness functionality
+let drawTarget = () => {
+  targetsArr.forEach(target => {
+    target.draw()
+  })
+}
+//Funcion que genere imagen aleatotria, y la posicione en x y y aleatoriamente
+const pill = new Target ('assets/pill.png', 200, 500)
+const player1 = new Player1('assets/ness-run-right.png',10,100)
+
+//EVENT LISTENERS
+
+
+document.onkeydown = e => {
+  //Map movements
+    switch(e.keyCode){
+      case 38:
+        player1.moveUp()
+          break;
+      case 40: 
+          player1.moveDown()
+          break;
+    }
+  }
+
+
+//1.- Event 1 changes background, draws screen and Ness, and gives Ness functionality
 startButton.addEventListener('click', () => {
-  //Change background
-  gameIntro.style.backgroundImage = "url(./assets/map.jpg)";
+  //Change background and appear canvas
+  gameArea.style.backgroundImage = 'url(assets/taco_truck_illustration-01_2x.jpg)'
+  canvas.style.display = 'block'
   //Disappear start screen elements
   startButton.style.display = 'none'
-  document.querySelector('.game-intro > h1').style.display = 'none'
-  document.querySelector('.game-intro > h2').style.display = 'none'
-  document.querySelector('.game-intro > img').style.display = 'none'
-  //Appear canvas so Ness can move around to pick world
-  document.querySelector('.screen2').style.display = 'block'
+  document.querySelector('.background > h1').style.display = 'none'
+  document.querySelector('.background > h2').style.display = 'none'
+  document.querySelector('.background > img').style.display = 'none'
   //Make Ness appear over Ironhack adress and draw map
-  drawScreen2()
   player1.draw()
 })
+
 
 //ACTIVATION AND HELPER FUNCTIONS
 
 const startGame = () => {
-  interval = setInterval(updateScreen2, 20)
+  interval = setInterval(gameLoop, 30)
  }
  const stopGame = () => {
    clearInterval(interval)
@@ -128,33 +149,17 @@ const startGame = () => {
    ctx.clearRect(0,0, canvas.width, canvas.height)
  }
 
-const drawScreen2 = () => {
-if (document.querySelector('.screen2').style.display === 'block'){
-  //dibujar cuadrado en Ironhack
-  ctx.fillStyle = 'red'
-  ctx.fillRect(116,65,3,3)
-  //dibujar lineas a mundos con sus circulos
-  ctx,fillStyle = 'black'
-  //Movimiento de Ironhack a el buen pastor
-  ctx.moveTo(115,68)
-  ctx.lineTo(104, 105)
-  ctx.stroke()
-  //Movimiento hacia tqueria el gato volador y cuadrado rojo en lugar
-  ctx.moveTo(103, 111)
-  ctx.lineTo(94, 144)
-  ctx.stroke()
-  //With the whole map drawn, draw Ness in IronHack location
-}
-}
-function updateScreen2() {
-  
-    frames += 1
-    startGame()
-    clearCanvas()
-    drawScreen2()
-    player1.draw()
+function gameLoop() {
+      frames += 1
+      startGame()
+      clearCanvas()
+      player1.draw()
+      generateTarget()
+      drawTarget()
+
     //drawObstacles()
     //updateObstacles()
     }
 
-updateScreen2()
+gameLoop()
+
