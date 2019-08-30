9b40598de60d1055b9af3794d5af8c8bd02dbd56
Emiliano <emilianopb92@gmail.com>
2019-08-30 13:03:15 -0500

    Working

--- main.js
+++ main.js
@@ -32,18 +32,18 @@ class Player1 {
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
   }
- moveDown() {
-   this.y +=10
- }
- moveUp() {
-  this.y -= 10
-}
-moveLeft() {
-  this.x -= 10
-}
-moveRight() {
-  this.x += 10
-}
+  moveDown() {
+    this.y += 10
+  }
+  moveUp() {
+    this.y -= 10
+  }
+  moveLeft() {
+    this.x -= 10
+  }
+  moveRight() {
+    this.x += 10
+  }
   top() {
     return this.y
   }
@@ -54,7 +54,7 @@ moveRight() {
     return this.x
   }
   right() {
-    return this.x + this.width 
+    return this.x + this.width
   }
   isTouching(target) {
     return (
@@ -68,7 +68,7 @@ moveRight() {
 }
 //1.- Objects
 class Target {
-  constructor (img, x, y) {
+  constructor(img, x, y) {
     this.x = x
     this.y = y
     this.height = 25
@@ -81,13 +81,13 @@ class Target {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
   }
   top() {
-    return this.y 
+    return this.y
   }
   bottom() {
     return this.y + this.height
   }
   left() {
-    return this.x 
+    return this.x
   }
   right() {
     return this.x + this.width
@@ -96,39 +96,39 @@ class Target {
   //  x = Math.round(Math.random() * canvas.width - targetLength);
   //  y = Math.round(Math.random() * canvas.height - targetLength)
   //}
-  }
+}
 
 
-const player1 = new Player1('assets/player1.png',10,100)
+const player1 = new Player1('./assets/player1.png', 10, 100)
 
 //EVENT LISTENERS
 
 
 document.onkeydown = e => {
   //Map movements
-    switch(e.keyCode){
-      case 38:
-        player1.moveUp()
-          break;
-      case 40: 
-          player1.moveDown()
-          break;
-      case 37:
-        player1.moveLeft()
-        break;
-      case 39:
-        player1.moveRight()
-        break;
-      case 81:
-        score = 14;
-    }
+  switch (e.keyCode) {
+    case 38:
+      player1.moveUp()
+      break;
+    case 40:
+      player1.moveDown()
+      break;
+    case 37:
+      player1.moveLeft()
+      break;
+    case 39:
+      player1.moveRight()
+      break;
+    case 81:
+      score = 14;
   }
+}
 
 
 //1.- Event 1 changes background, draws screen and Ness, and gives Ness functionality
 startButton.addEventListener('click', () => {
   //Change background and appear canvas
-  canvas.style.backgroundImage = 'url(assets/output-onlinepngtools.png)'
+  canvas.style.backgroundImage = 'url(./assets/output-onlinepngtools.png)'
   canvas.style.display = 'block'
   //Disappear start screen elements
   startButton.style.display = 'none'
@@ -146,59 +146,61 @@ startButton.addEventListener('click', () => {
 
 const startGame = () => {
   interval = setInterval(gameLoop, 20)
- }
- const stopGame = () => {
-   if (score === 10) {
-   clearInterval(interval)
-   interval = null
-   }
- }
- 
- const clearCanvas = () => {
-   ctx.clearRect(0,0, canvas.width, canvas.height)
- }
- const generateTarget = () => {
-   //const good = './assets/pill.png'
-   //const bad = './assets/sick-taco_burned.png'
-  const yolo = ['assets/good-pill.png', 
-  'assets/good-taco.png',
-  'assets/bad-tacoo.png',
-  'assets/bad-germ.png']
+}
+const stopGame = () => {
+  if (score === 10) {
+    clearInterval(interval)
+    interval = null
+  }
+}
+
+const clearCanvas = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+const generateTarget = () => {
+  //const good = './assets/pill.png'
+  //const bad = './assets/sick-taco_burned.png'
+  const yolo = ['./assets/good-pill.png',
+    './assets/good-taco.png',
+    './assets/bad-tacoo.png',
+    './assets/bad-germ.png']
 
   let x = canvas.width
   let y = canvas.height
 
-  if (frames % 3000  === 0 ) {
-    let randX = Math.floor((Math.random() * x ) + 5)
-    let randY = Math.floor((Math.random() * y ) + 5)
-    let randTarget = Math.floor(Math.random() * 4 )
+  if (frames % 3000 === 0) {
+    let randX = Math.floor((Math.random() * x) + 5)
+    let randY = Math.floor((Math.random() * y) + 5)
+    let randTarget = Math.floor(Math.random() * 4)
     targets.push(new Target(yolo[randTarget], randX, randY))
   }
 }
 
 const drawTarget = () => {
-  if(targets.length !== 0){targets.forEach(element => {
-    element.draw()
-  })}
+  if (targets.length !== 0) {
+    targets.forEach(element => {
+      element.draw()
+    })
+  }
 }
 const collision = () => {
-  targets.forEach(( element => {
+  targets.forEach((element => {
     let indexOfEl = targets.indexOf(element)
-    console.log(element.id)
-    if (player1.isTouching(element) && element.img.src === 'assets/good-taco.png') {
+
+    if (player1.isTouching(element) && element.img.src.includes('good-taco.png')) {
+      console.log("Hey")
       targets.splice(indexOfEl, 1)
       score += 1
-    
-    }else if (player1.isTouching(element) && element.img.src === 'assets/good-pill.png') {
+    } else if (player1.isTouching(element) && element.img.src.includes('good-pill.png')) {
       targets.splice(indexOfEl, 1)
       score += 2
-    }else if (player1.isTouching(element) && element.img.src === 'assets/bad-germ.png') {
+    } else if (player1.isTouching(element) && element.img.src.includes('bad-germ.png')) {
       targets.splice(indexOfEl, 1)
       score -= 2
-    } else if (player1.isTouching(element) && element.img.src === 'assets/bad-tacoo.png') {
+    } else if (player1.isTouching(element) && element.img.src.includes('bad-tacoo.png')) {
       targets.splice(indexOfEl, 1)
       score -= 1
-  }
+    }
   }))
 }
 function drawScore() {
@@ -206,45 +208,42 @@ function drawScore() {
   ctx.fillStyle = 'black'
   ctx.font = '16px Courier'
   ctx.fillText(`Score: ${score}`, 10, 20)
-  }
+}
 
-  function winScreen() {
-  
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    canvas.style.display = 'none'
-    gameArea.backgroundColor = 'black'
-    document.querySelector('.background > img').style.display = 'block'
-    document.querySelector('.background > h1').innerHTML = 'YOU WIN!!'
-    document.querySelector('.background > h1').style.fontSize = '10rem'
-    document.querySelector('.background > h1').style.padding = '50px 0 0 0'
-    document.querySelector('.background > h1').style.display = 'block'
-    document.querySelector('.background > h1').style.padding = '50px 0 0 0' 
-    document.querySelector('.background > h2').innerHTML = 'Press ctrl + r to play again'
-    document.querySelector('.background > h2').style.display = 'block'  
-    clearInterval(interval)
+function winScreen() {
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  canvas.style.display = 'none'
+  gameArea.backgroundColor = 'black'
+  document.querySelector('.background > img').style.display = 'block'
+  document.querySelector('.background > h1').innerHTML = 'YOU WIN!!'
+  document.querySelector('.background > h1').style.fontSize = '10rem'
+  document.querySelector('.background > h1').style.padding = '50px 0 0 0'
+  document.querySelector('.background > h1').style.display = 'block'
+  document.querySelector('.background > h1').style.padding = '50px 0 0 0'
+  document.querySelector('.background > h2').innerHTML = 'Press ctrl + r to play again'
+  document.querySelector('.background > h2').style.display = 'block'
+  clearInterval(interval)
 
 }
 
 function youWin() {
-    if (score >= 15) {
-        return winScreen()
-    }
+  if (score >= 15) {
+    return winScreen()
+  }
 }
 
 
 
 function gameLoop() {
-      frames += 1
-      startGame()
-      clearCanvas()
-      player1.draw()
-      generateTarget()
-      drawTarget()
-      collision()
-      drawScore()
-      stopGame()
-      youWin()
-    }
-
-
-
+  frames += 1
+  startGame()
+  clearCanvas()
+  player1.draw()
+  generateTarget()
+  drawTarget()
+  collision()
+  drawScore()
+  stopGame()
+  youWin()
+}
